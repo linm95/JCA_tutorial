cd71090c1be59b84836ba587b54f474daf4407a3
linm95 <linm95@utexas.edu>
2017-11-24 12:26:21 -0600

    lm

---
+++ src/tutorial/Exercise2.java
@@ -0,0 +1,11 @@
+package tutorial;
+
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EncryptAndDecrypt ead = new EncryptAndDecrypt();
+		ead.printClearText("/home/linm/Projects/JCA_tutorial/key", "/home/linm/Projects/JCA_tutorial/encrypted.txt");
+	}
+
+}
---
+++ src/tutorial/Exercise3.java
@@ -0,0 +1,43 @@
+package tutorial;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SignatureMatch sm = new SignatureMatch();
+		//sm.printData();
+		byte[][] sigs= new byte[3][];
+		byte[][] data = new byte[3][];
+		PublicKey[] keys = new PublicKey[3];
+		for(int i = 0; i < 3; i++) {
+			try {
+				sigs[i] = Files.readAllBytes(Paths.get("/home/linm/Projects/JCA_tutorial/sig" + i));
+				data[i] = Files.readAllBytes(Paths.get("/home/linm/Projects/JCA_tutorial/data" + i));
+				byte[] encodedPubKey = Files.readAllBytes(Paths.get("/home/linm/Projects/JCA_tutorial/publickey" + i));
+				X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encodedPubKey);
+				KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+				keys[i] = keyFactory.generatePublic(pubKeySpec);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		sm = new SignatureMatch();
+		for(int i = 0; i < 3; i ++) {
+			for(int j = 0; j  < 3; j++) {
+				for(int k = 0; k < 3; k ++) {
+					if(sm.verifySignature(keys[i], sigs[j], data[k])) {
+						System.out.println("publickey" + i + ", sig" + j + ", data" + k +" match");
+					}
+				}
+			}
+		}
+	}
+
+}
